478373646009202a3347f14a78ebb7d130202ae5
Jack Walker <xjackwalker@gmail.com>
2023-04-23 15:48:33 +1000

    bug fixed

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component,  Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -6,16 +6,17 @@ import { Recipe } from '../../recipe.model';
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent {
-  @Input () recipe: Recipe;
-  @Output () recipeSelected = new EventEmitter () ;
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter();
 
-  constructor () { }
+  constructor() { }
 
-  ngOnInit (): void {
+  ngOnInit(): void {
   }
 
-  onSelected() {
-    this.recipeSelected.emit
+  onSelected(){
+    this.recipeSelected.emit();
   }
+
 }

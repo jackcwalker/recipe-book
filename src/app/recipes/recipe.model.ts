96fbed860fa5a8d6832d3d46005de929b5dae0b6
Jack Walker <xjackwalker@gmail.com>
2023-04-27 21:00:09 +1000

    added image path to firebase

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -24,9 +24,12 @@ export class Recipe {
         cookTime: number,
         author: string,
     ) {
+        const base = "https://firebasestorage.googleapis.com/v0/b/recipe-book-85758.appspot.com/o/";
+        const key = "?alt=media&token=76e5d494-980e-45e2-b708-0a2118f78770"
+
         this.name = name;
         this.description = desc;
-        this.imagePath = imagePath;
+        this.imagePath = base + imagePath + key;
         this.ingredients = ingredients;
         this.recipeType = recipeType;
         this.serves = serves;

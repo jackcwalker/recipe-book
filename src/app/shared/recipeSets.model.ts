291a69d3d11d5d9f7fe8324a5d9e6f75818a51ff
Jack Walker <xjackwalker@gmail.com>
2023-05-03 23:11:36 +1000

    added new tags and users list

--- src/app/shared/recipeSets.model.ts
+++ src/app/shared/recipeSets.model.ts
@@ -1,4 +1,6 @@
-export var tags : string[] = ['Healthy', 'High Protein', 'Vegan', 'Quick', 'Gluten-Free'].sort()
+export var tags : string[] = ['Healthy', 'High Protein', 'Vegan', 'Quick', 'Gluten-Free', 'Asian', 'Indian', 'Mexican', 'Italian', 'British', 'Mediterranean'].sort()
+
+export var users : string[] = ['Jack', 'Naomi', 'George', 'Emily', 'Paul', 'Trisha', 'Isaac', 'Maren', 'Siân'].sort()
 
 export enum recipeType {
     smoothie = 'Smoothie',

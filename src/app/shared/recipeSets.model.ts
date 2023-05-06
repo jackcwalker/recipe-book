b7118744ba95af2b5358420425cd8c7dad9fa80f
Jack Walker <xjackwalker@gmail.com>
2023-05-06 10:54:35 +1000

    added legume free emoji

--- src/app/shared/recipeSets.model.ts
+++ src/app/shared/recipeSets.model.ts
@@ -1,4 +1,4 @@
-export var tags : string[] = ['Healthy 😇', 'High Protein 💪', 'Vegan 🌱', 'Quick ⏰', 'Gluten-Free ❌🍞', 'Asian 🍚', 'Indian 🇮🇳', 'Mexican 🌮', 'Italian 🍝', 'British 🥔', 'Mediterranean 🫒', 'Chocolate 🍫', 'Vanilla 🍨', 'Banana 🍌'].sort()
+export var tags : string[] = ['Healthy 😇', 'High Protein 💪', 'Vegan 🌱', 'Quick ⏰', 'Gluten-Free ❌🍞', 'Legume-Free ❌🫘', 'Asian 🍚', 'Indian 🇮🇳', 'Mexican 🌮', 'Italian 🍝', 'British 🥔', 'Mediterranean 🫒', 'Chocolate 🍫', 'Vanilla 🍨', 'Banana 🍌'].sort()
 
 export var users : string[] = ['Jack', 'Naomi', 'George', 'Emily', 'Paul', 'Trisha', 'Isaac', 'Maren', 'Siân'].sort()
 
